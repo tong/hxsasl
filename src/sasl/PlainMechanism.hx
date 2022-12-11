d214cf272cbaae5ac066d85e5f7e77ce329a3194
tong <tong@disktree.net>
2022-12-11 23:57:44 +0100

    Make authcid usage optional in plain mechanism

--- src/sasl/PlainMechanism.hx
+++ src/sasl/PlainMechanism.hx
@@ -3,34 +3,37 @@ package sasl;
 /*
 	[The PLAIN Simple Authentication and Security Layer (SASL) Mechanism](https://www.ietf.org/rfc/rfc4616.txt)
 
-	The PLAIN mechanism should not be used without adequate data security protection as this mechanism affords no integrity or confidentiality protections itself.
+	The PLAIN mechanism should not be used without adequate data security
+    protection as this mechanism affords no integrity or confidentiality
+    protections itself.
 
-	By default, implementations SHOULD advertise and make use of the PLAIN mechanism only when adequate data security services are in place.
+	By default, implementations SHOULD advertise and make use of the PLAIN
+    mechanism only when adequate data security services are in place.
 */
 class PlainMechanism implements Mechanism {
 
 	public final name = 'PLAIN';
+    
+    public var useAuthcid : Bool;
 
-	public function new() {}
+	public function new(useAuthcid = true) {
+        this.useAuthcid = useAuthcid;
+    }
 
-	public function createAuthenticationText( authzid : String, authcid : String, password : String ) : String {
-		
-		if( authzid == null || authcid == null )
-			return null;
-		
-		// //TODO authzid
-
-		// var z = String.fromCharCode( 0 );
-		// return '$z$authcid$z$password';
-		
-		//Buffer.from(`\0${params.user}\0${params.password}`);
-		
-		var b = new StringBuf();
-		b.add( String.fromCharCode( 0 ) );
-		b.add( authcid );
-		b.add( String.fromCharCode( 0 ) );
-		b.add( password );
-		return b.toString();
+	public function createAuthenticationText(authzid: String, authcid: String, password: String) : String {
+        //var z = String.fromCharCode( 0 );
+		//TODO authzid
+		//return '$z$authzid$z$password';
+        var b = new StringBuf();
+		b.add(String.fromCharCode(0));
+        b.add(authzid);
+        if(useAuthcid && authcid != null) {
+            b.add(String.fromCharCode(0));
+		    b.add(authcid);
+        }
+		b.add(String.fromCharCode(0));
+		b.add(password);
+        return b.toString();
 	}
 
 	/**
