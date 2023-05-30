d5355132580a39a8aee81bd58612fb0cc1d1b7b8
tong <tong@disktree.net>
2023-05-30 18:58:54 +0200

    Cleanup

--- src/sasl/PlainMechanism.hx
+++ src/sasl/PlainMechanism.hx
@@ -3,11 +3,11 @@ package sasl;
 /**
 	[The PLAIN Simple Authentication and Security Layer (SASL) Mechanism](https://www.ietf.org/rfc/rfc4616.txt)
 
-	The PLAIN mechanism should not be used without adequate data security
+	The `PLAIN` mechanism should not be used without adequate data security
     protection as this mechanism affords no integrity or confidentiality
     protections itself.
 
-	By default, implementations SHOULD advertise and make use of the PLAIN
+	By default, implementations **SHOULD** advertise and make use of the `PLAIN`
     mechanism only when adequate data security services are in place.
 **/
 @rfc(4616)
@@ -21,34 +21,43 @@ class PlainMechanism implements Mechanism {
         this.useAuthcid = useAuthcid;
     }
 
+    /**
+        @param authzid Authorization identity
+        @param authcid Authentication identity
+        @param password Password
+    **/
 	public function createAuthenticationText(authzid: String, authcid: String, password: String) : String {
-		//TODO authzid
-		//return '$z$authzid$z$password';
         if(authzid == null)
             return null;
-        var b = new StringBuf();
-		b.add(String.fromCharCode(0));
-        b.add(authzid);
+        //TODO: string prep
+        // final authcid_p = StrinPrep.prep(authcid, true);
+        // final password_p = StrinPrep.prep(password, true);
+        // if(authcid_p == null || password_p == null 
+        //     || authcid_p.length==0 || password_p.length==0)
+        //     return null;
+        final buf = new StringBuf();
+		buf.add(String.fromCharCode(0));
+        buf.add(authzid);
         if(useAuthcid && authcid != null) {
-            b.add(String.fromCharCode(0));
-		    b.add(authcid);
+            buf.add(String.fromCharCode(0));
+		    buf.add(authcid);
         }
-		b.add(String.fromCharCode(0));
-		b.add(password);
-        return b.toString();
+		buf.add(String.fromCharCode(0));
+		buf.add(password);
+        return buf.toString();
 	}
 
 	/**
 		This mechanism will never get a challenge from the server.
 	**/
-	public function createChallengeResponse( challenge : String ) : String {
+	public function createChallengeResponse(challenge: String) : String {
 		return null;
 	}
 
-	public function handleAuthenticationText( text : String ) : Array<String> {
+	public function handleAuthenticationText(text: String) : Array<String> {
 		if( text == null )
 			return [];
-		var a = text.split( String.fromCharCode( 0 ) );
+        final a = text.split(String.fromCharCode(0));
 		a.shift();
 		return a;
 	}
--- test/Unit.hx
+++ test/Unit.hx
@@ -1,11 +1,10 @@
-
 function main() {
 	var runner = new utest.Runner();
-	//runner.addCase( new TestPrep() );
-	runner.addCase( new TestAnonymousMechanism() );
-	runner.addCase( new TestMD5Mechanism() );
-	runner.addCase( new TestPlainMechanism() );
-	runner.addCase( new TestSCRAMSHA1Mechanism() );
-	utest.ui.Report.create( runner );
+	//runner.addCase(new TestStringPrep());
+	runner.addCase(new TestAnonymousMechanism());
+	runner.addCase(new TestMD5Mechanism());
+	runner.addCase(new TestPlainMechanism());
+	runner.addCase(new TestSCRAMSHA1Mechanism());
+	utest.ui.Report.create(runner);
 	runner.run();
 }
